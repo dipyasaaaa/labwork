06923d067a8b9955d06bb25b9efb202c710ce6dc
Dibyashaa Pany <154058383+dipyasaaaa@users.noreply.github.com>
2025-03-12 08:38:31 +0530

    Add files via upload

---
+++ DAA/exp1.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+using namespace std;
+
+// Function to perform Insertion Sort
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1], that are greater than key,
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout<<"Enter the number of elements in the array:"<<endl;
+    cin>>n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: " << endl;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];}
+
+
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
